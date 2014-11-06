fb4e27e2e2d450dbcd9119cb763475a37b4a1ca2
Yuki Suzuki <s12bte@gmail.com>
2014-11-06 23:21:27 +0900

    Grunt-contribを必要な物だけにした

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,6 @@ module.exports = function(grunt){
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks("grunt-contrib-connect");
-  grunt.loadNpmTasks("grunt-contrib-copy");
   grunt.loadNpmTasks("grunt-contrib-symlink");
 
   grunt.initConfig({
